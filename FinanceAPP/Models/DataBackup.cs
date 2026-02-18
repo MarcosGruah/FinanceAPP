2eef75956d5b5ac6170f22dfff2ced683e66a963
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2026-02-18 08:08:55 -0300

    Add DataBackup class for transactions and categories
    Introduced FinanceAPP.Models.DataBackup with properties for storing lists of Transaction and Category objects, both initialized as empty lists. This will facilitate data backup and restore functionality.

---
+++ FinanceAPP/Models/DataBackup.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceAPP.Models
+{
+    public class DataBackup
+    {
+        public List<Transaction> Transactions { get; set; } = new();
+        public List<Category> Categories { get; set; } = new();
+    }
+}
